83444417aac9827cece2f0306c589e6fb0e6e158
Melinda S <roseofgold@gmail.com>
2019-05-17 18:38:27 -0700

    Start ToDo class/model

---
+++ src/Model/Todo.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Model;
+
+class ToDo
+{
+    protected $database;
+
+    public function __construct(\PDO $database)
+    {
+        $this->database = $database;
+    }
+
+    public function getAllToDos()
+    {
+        $statement = $this->database->prepare(
+            'SELECT * FROM tasks ORDER BY id'
+        );
+        $statement->execute();
+        return $statement->fetchAll();
+    }
+}
