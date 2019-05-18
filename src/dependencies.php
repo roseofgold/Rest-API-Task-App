26c11a1cc31b6f90faef501a133e44f291c7e86f
Melinda S <roseofgold@gmail.com>
2019-05-17 17:53:24 -0700

    add api and database dependencies

--- src/dependencies.php
+++ src/dependencies.php
@@ -19,4 +19,19 @@ return function (App $app) {
         $logger->pushHandler(new \Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
         return $logger;
     };
+
+    // api
+    $container['api'] = function ($c){
+        $api = $c->get('settings')['api'];
+        return $api;
+    };
+
+    // database 
+    $container['db'] = function ($c) {
+        $db = $c->get('settings')['db'];
+        $pdo = new PDO($db['dsn'] . ':' . $db['database']);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $pdo;
+    };
 };
