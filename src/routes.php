0717a968eca9b7c5b7ce44efa4f84abdbd64b38c
Melinda S <roseofgold@gmail.com>
2019-05-17 18:38:06 -0700

    update to show add ToDos

--- src/routes.php
+++ src/routes.php
@@ -8,10 +8,7 @@ return function (App $app) {
     $container = $app->getContainer();
 
     $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
-        // Sample log message
-        $container->get('logger')->info("Slim-Skeleton '/' route");
-
-        // Render index view
-        return $container->get('renderer')->render($response, 'index.phtml', $args);
+        $result = $this->todo->getAllToDos();
+        return $response->withJson($result, 200, JSON_PRETTY_PRINT);
     });
 };
