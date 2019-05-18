db3fa2ee6310a1e5d77e289ff1f8d7a5122de0c3
Melinda S <roseofgold@gmail.com>
2019-05-17 21:42:40 -0700

    add functions for CRUD

--- src/Model/Todo.php
+++ src/Model/Todo.php
@@ -18,4 +18,47 @@ class ToDo
         $statement->execute();
         return $statement->fetchAll();
     }
+
+    public function getToDo($task_id)
+    {
+        $statement = $this->database->prepare(
+            'SELECT * FROM tasks WHERE id=:id'
+        );
+        $statement->bindParam('id', $task_id);
+        $statement->execute();
+        return $statement->fetch();
+    }
+    
+    public function createToDo($data)
+    {
+        $statement = $this->database->prepare(
+            'INSERT INTO tasks (task,status) VALUES (:task,:status)'
+        );
+        $statement->bindParam('task', $data['task']);
+        $statement->bindParam('status', $data['status']);
+        $statement->execute();
+        return $this->getToDo($this->database->lastInsertID());
+    }
+    
+    public function updateToDo($data)
+    {
+        $statement = $this->database->prepare(
+            'UPDATE tasks SET task=:task, status=:status WHERE id=:id'
+        );
+        $statement->bindParam('task', $data['task']);
+        $statement->bindParam('status', $data['status']);
+        $statement->bindParam('id', $data['id']);
+        $statement->execute();
+        return $this->getToDo($data['id']);
+    }
+    
+    public function deleteToDo($task_id)
+    {
+        $statement = $this->database->prepare(
+            'DELETE FROM tasks WHERE id=:id'
+        );
+        $statement->bindParam('id', $task_id);
+        $statement->execute();
+        return ['message' => 'task was deleted'];
+    }
 }
