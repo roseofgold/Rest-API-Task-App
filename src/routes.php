b1b6fa334b1fd53d24d68a476f1356a51dc67075
Melinda S <roseofgold@gmail.com>
2019-05-17 21:43:13 -0700

    add endpoints and group routes

--- src/routes.php
+++ src/routes.php
@@ -7,12 +7,23 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
-    $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
-        // Sample log message
-        $container->get('logger')->info("Slim-Skeleton '/' route");
-
-        // return all todos
-        $result = $this->todo->getAllToDos();
+    $app->get('/', function (Request $request, Response $response, array $args) use ($container) {
+        $endpoints = [
+            'all tasks' => $this->api['api_url'] . '/tasks',
+            'single task' => $this->api['api_url'] . '/tasks/{task_id}',
+        ];
+        $result = [
+            'endpoints' => $endpoints,
+            'version' => $this->api['version'],
+            'timestamp' => time(),
+        ];
         return $response->withJson($result, 200, JSON_PRETTY_PRINT);
+    });
+    $app->group('/api/v1/tasks', function() use($app) {
+        $app->get('', function (Request $request, Response $response, array $args) use ($container) {
+            // return all todos
+            $result = $this->todo->getAllToDos();
+            return $response->withJson($result, 200, JSON_PRETTY_PRINT);
+        });    
     });
 };
